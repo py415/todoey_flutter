153272944f969af525a081c73650deecb9baea67
Philip Yu <pyu3@mail.sfsu.edu>
2019-08-17 21:28:50 -0700

    Abstract ListView Widgets into separate classes

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
@@ -8,6 +9,9 @@ class TasksScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
+        onPressed: () {
+          // Add new task
+        },
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -54,6 +58,7 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -61,6 +66,7 @@ class TasksScreen extends StatelessWidget {
                   topRight: Radius.circular(20.0),
                 ),
               ),
+              child: TasksList(),
             ),
           )
         ],
---
+++ lib/widgets/tasks_tile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TasksTile extends StatelessWidget {
+  bool checkbox = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('This is a task.'),
+      trailing: Checkbox(
+        value: checkbox,
+        onChanged: (value) {
+          value = true;
+          checkbox = value;
+        },
+      ),
+    );
+  }
+}
