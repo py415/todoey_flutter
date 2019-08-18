149746a0ac55cfe159dc1d143b98df068238ab70
Philip Yu <pyu3@mail.sfsu.edu>
2019-08-17 22:36:55 -0700

    Add custom callback to handle ListView checkbox

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TasksTile extends StatelessWidget {
-  bool checkbox = false;
+class TasksTile extends StatefulWidget {
+  @override
+  _TasksTileState createState() => _TasksTileState();
+}
+
+class _TasksTileState extends State<TasksTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: checkbox,
-        onChanged: (checkbox) {
-          checkbox = true;
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
         },
       ),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: checkboxState,
+        onChanged: toggleCheckboxState);
   }
 }
